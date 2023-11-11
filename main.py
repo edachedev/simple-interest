a76179999bbd6717132a5e36a8f055ea0a7f332e
edachedev <victor.edache@binghamuni.edu.ng>
2023-11-11 22:32:16 +0100

    added ascii art

--- main.py
+++ main.py
@@ -1,6 +1,25 @@
-business_name = "Yusuf and Sons"
+business_logo = """
+                                                                                                                                      
+                                          ,--.                                    ,--.                                            
+                        ,--.   ,--. ,---. |  | ,---. ,---. ,--,--,--. ,---.     ,-'  '-. ,---.                                    
+                        |  |.'.|  || .-. :|  || .--'| .-. ||        || .-. :    '-.  .-'| .-. |                                   
+                        |   .'.   |\   --.|  |\ `--.' '-' '|  |  |  |\   --.      |  |  ' '-' '                                   
+                        '--'   '--' `----'`--' `---' `---' `--`--`--' `----'      `--'   `---'                                    
+                                                 ,---.                       ,--.                          ,--.                   
+            ,--. ,--.,--.,--. ,---. ,--.,--./  .-'     ,--,--.,--,--,  ,-|  |     ,---.  ,---. ,--,--, |  |,---.              
+             \  '  / |  ||  |(  .-' |  ||  ||  `-,    ' ,-.  ||      \' .-. |    (  .-' | .-. ||      \`-'(  .-'              
+              \   '  '  ''  '.-'  `)'  ''  '|  .-'    \ '-'  ||  ||  |\ `-' |    .-'  `)' '-' '|  ||  |   .-'  `)             
+            .-'  /    `----' `----'  `----' `--'       `--`--'`--''--' `---'     `----'  `---' `--''--'   `----'              
+            `---'                                                                                                             
+,--.          ,--.                               ,--.                    ,--.              ,--.          ,--.                 
+`--',--,--, ,-'  '-. ,---. ,--.--. ,---.  ,---.,-'  '-.     ,---. ,--,--.|  | ,---.,--.,--.|  | ,--,--.,-'  '-. ,---. ,--.--. 
+,--.|      \'-.  .-'| .-. :|  .--'| .-. :(  .-''-.  .-'    | .--'' ,-.  ||  || .--'|  ||  ||  |' ,-.  |'-.  .-'| .-. ||  .--' 
+|  ||  ||  |  |  |  \   --.|  |   \   --..-'  `) |  |      \ `--.\ '-'  ||  |\ `--.'  ''  '|  |\ '-'  |  |  |  ' '-' '|  |    
+`--'`--''--'  `--'   `----'`--'    `----'`----'  `--'       `---' `--`--'`--' `---' `----' `--' `--`--'  `--'   `---' `--'    
+                                                                                                                                                                                
+"""
 
-print(f"Welcome to {business_name} Interest Calculator!")
+print(business_logo)
 principal = float(input("Enter the initial principal amount: "))
 rate = float(input("Enter the interest rate (in decimal form): "))
 time_periods = int(input("Enter the number of time periods elapsed: "))
@@ -12,5 +31,5 @@ simple_interest = principal * (1 + rate * time_periods)
 # Calculate Compound Interest
 compound_interest = principal * (1 + rate / frequency) ** (frequency * time_periods)
 
-print(f"\nSimple Interest for {business_name} is: ${simple_interest:.2f}")
-print(f"Compound Interest for {business_name} is: ${compound_interest:.2f}")
+print(f"\nSimple Interest for {business_logo} is: ${simple_interest:.2f}")
+print(f"Compound Interest for {business_logo} is: ${compound_interest:.2f}")
